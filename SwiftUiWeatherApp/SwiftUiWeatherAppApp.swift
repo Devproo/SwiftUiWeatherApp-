a733f8249b89ecc65bd8db7d290bc24ef66399fd
ipeerless <ipeerless0@gmail.com>
2025-01-17 19:41:12 +0200

    Initial Commit

---
+++ SwiftUiWeatherApp/SwiftUiWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiWeatherAppApp.swift
+//  SwiftUiWeatherApp
+//
+//  Created by ipeerless on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
